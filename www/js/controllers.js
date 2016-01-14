193593309b389140a09bfd172ec77cac6f4a1f81
Mario333 <msanchez26@iesnumancia.cat>
2016-01-14 12:01:19 +0100

    caladuba

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,7 +3,7 @@
 */
 
 $JSView.controller = {
-    sol: function(e){
+    inici: function(e){
 		$JSView.dataView({},e)
 	},
     sol: function(e){
@@ -12,11 +12,29 @@ $JSView.controller = {
     mercuri: function(e){
 		$JSView.dataView({},e)
 	},
-	
     venus: function(e){
+		$JSView.dataView({},e)
+	},
+    terra: function(e){
+		$JSView.dataView({},e)
+	},
+    mart: function(e){
+		$JSView.dataView({},e)
+	},
+    jupiter: function(e){
+		$JSView.dataView({},e)
+	},
+    ura: function(e){
+		$JSView.dataView({},e)
+	},
+    neptu: function(e){
+		$JSView.dataView({},e)
+	},
+    saturn: function(e){
 		$JSView.dataView({},e)
 	},
     modalA: function(e){
 		$JSView.dataView({},e)	
 	}
 }
+
